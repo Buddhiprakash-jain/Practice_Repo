1ce565eeff67f831275bd38cb853011b73e1e025
Buddhiprakash jain <71218910+Buddhiprakash-jain@users.noreply.github.com>
2023-01-06 23:12:46 +0530

    Update gcp_stopvm_python.py

--- gcp_stopvm_python.py
+++ gcp_stopvm_python.py
@@ -3,4 +3,4 @@ import googleapiclient.discovery
 def stop_vm(request):
     compute = googleapiclient.discovery.build('compute', 'v1')
     result = compute.instances().stop(project='basic-tube-373302', zone='asia-southeast1-b', instance='pyvm').execute()
-    return result
+    return "VM Stopping.."
