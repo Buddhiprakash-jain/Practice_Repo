2c54e49e8dc2e6389b8556b49d798fd706981716
Buddhiprakash jain <71218910+Buddhiprakash-jain@users.noreply.github.com>
2023-01-09 22:11:10 +0530

    Update gcp_deletevm_python.py

--- gcp_deletevm_python.py
+++ gcp_deletevm_python.py
@@ -6,9 +6,17 @@ import time
 import googleapiclient.discovery
 from six.moves import input
 
-compute = googleapiclient.discovery.build('compute', 'v1')
+#compute = googleapiclient.discovery.build('compute', 'v1')
+def delete_instance(request):
+    compute = googleapiclient.discovery.build('compute', 'v1')
+    def list_instances(compute, project, zone):
+        result = compute.instances().list(project=project, zone=zone).execute()
+        return result['items'] if 'items' in result else None
+        # [END list_instances]
 
-def delete_instance(compute, project, zone, name):
-  result = compute.instances().delete(project=project,zone=zone,instance=name).execute()
-  return "VM Deleting.."
-delete_instance(compute,'basic-tube-373302' ,'asia-southeast1-b', 'pyvm')
+    x = list_instances(compute,'basic-tube-373302' ,'asia-southeast1-b')
+    for i in range(len(x)):
+	        if (x[i]["name"]) == "pyvm":
+                 result = compute.instances().delete(project='basic-tube-373302',zone='asia-southeast1-b',instance='pyvm').execute()
+                 return "VM Deleting"
+    return "Vm not found.."
