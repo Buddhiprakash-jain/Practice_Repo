92a865c0479c0cf08b859ed8900586c3317ce80f
Buddhiprakash jain <71218910+Buddhiprakash-jain@users.noreply.github.com>
2023-01-09 22:11:47 +0530

    Update gcp_startvm_python.py

--- gcp_startvm_python.py
+++ gcp_startvm_python.py
@@ -1,6 +1,26 @@
-import googleapiclient.discovery
+import argparse
+import os
+import time
 
+# pip install google-api-python-client
+import googleapiclient.discovery
+from six.moves import input
+
+#compute = googleapiclient.discovery.build('compute', 'v1')
 def start_vm(request):
     compute = googleapiclient.discovery.build('compute', 'v1')
-    result = compute.instances().start(project='basic-tube-373302', zone='asia-southeast1-b', instance='pyvm').execute()
-    return "VM Starting.."
+    def list_instances(compute, project, zone):
+        result = compute.instances().list(project=project, zone=zone).execute()
+        return result['items'] if 'items' in result else None
+        # [END list_instances]
+
+    x = list_instances(compute,'basic-tube-373302' ,'asia-southeast1-b')
+    for i in range(len(x)):
+	    if (x[i]["name"]) == "pyvm":
+            	if (x[i]["status"]) == "RUNNING":
+                	return "VM already in running state.."
+            	else:
+                	result = compute.instances().start(project='basic-tube-373302', zone='asia-southeast1-b', instance='pyvm').execute()
+                	return "VM Starting.."
+    return "Vm not found.."
+        
