cc37b438fd10b6aff6aad3a43f63a036d43bbea1
Buddhiprakash jain <71218910+Buddhiprakash-jain@users.noreply.github.com>
2023-01-10 00:14:02 +0530

    Update gcp_startvm_python.py

--- gcp_startvm_python.py
+++ gcp_startvm_python.py
@@ -9,18 +9,20 @@ from six.moves import input
 #compute = googleapiclient.discovery.build('compute', 'v1')
 def start_vm(request):
     compute = googleapiclient.discovery.build('compute', 'v1')
+    name = request.args.get('name')
+    zone = request.args.get('zone')
     def list_instances(compute, project, zone):
         result = compute.instances().list(project=project, zone=zone).execute()
         return result['items'] if 'items' in result else None
         # [END list_instances]
 
-    x = list_instances(compute,'basic-tube-373302' ,'asia-southeast1-b')
+    x = list_instances(compute,'basic-tube-373302' ,zone)
     for i in range(len(x)):
-	    if (x[i]["name"]) == "pyvm":
+	    if (x[i]["name"]) == name:
             	if (x[i]["status"]) == "RUNNING":
-                	return "VM already in running state.."
+                	return "VM %s already in running state.." % name
             	else:
-                	result = compute.instances().start(project='basic-tube-373302', zone='asia-southeast1-b', instance='pyvm').execute()
-                	return "VM Starting.."
-    return "Vm not found.."
+                	result = compute.instances().start(project='basic-tube-373302', zone=zone, instance=name).execute()
+                	return "VM %s Starting.." % name
+    return "Vm %s not found.." % name
         
