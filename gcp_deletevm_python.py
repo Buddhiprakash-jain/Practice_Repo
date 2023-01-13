f0e933eb0ebd66f5e98fc4a6ca94abf037d862e7
Buddhiprakash jain <71218910+Buddhiprakash-jain@users.noreply.github.com>
2023-01-13 18:03:14 +0530

    Update gcp_deletevm_python.py

--- gcp_deletevm_python.py
+++ gcp_deletevm_python.py
@@ -21,13 +21,13 @@ def delete_instance(request):
         result = compute.instances().list(project=project, zone=zone).execute()
         return result['items'] if 'items' in result else None
         # [END list_instances]
-    sql = "insert into newtable (instanceName,operation,zone) values (%s,%s,%s)"
-    val = (name,"DELETE",zone)
-    mycursor.execute(sql, val)
-    mydb.commit()
     x = list_instances(compute,'basic-tube-373302' ,zone)
     for i in range(len(x)):
 	        if (x[i]["name"]) == name:
                  result = compute.instances().delete(project='basic-tube-373302',zone=zone,instance=name).execute()
+                 sql = "insert into newtable (instanceName,operation,zone,returnmessage) values (%s,%s,%s,%s)"
+                 val = (name,"Delete",zone,"VM %s Delete" % name)
+                 mycursor.execute(sql, val)
+                 mydb.commit()
                  return "VM %s Deleting" % name
     return "VM %s not found.." % name
