1c90a9fbbf4c13e076fa6d93073b4ffde2a88314
Buddhiprakash jain <71218910+Buddhiprakash-jain@users.noreply.github.com>
2023-01-13 12:22:30 +0530

    Update gcp_deletevm_python.py

--- gcp_deletevm_python.py
+++ gcp_deletevm_python.py
@@ -1,11 +1,17 @@
 import argparse
 import os
 import time
-
+import mysql.connector
 # pip install google-api-python-client
 import googleapiclient.discovery
 from six.moves import input
-
+mydb = mysql.connector.connect(
+        host="34.142.173.243",
+        user="root",
+        password="buddhi",
+        database="mybpdb"
+            )
+mycursor = mydb.cursor()
 #compute = googleapiclient.discovery.build('compute', 'v1')
 def delete_instance(request):
     compute = googleapiclient.discovery.build('compute', 'v1')
@@ -15,7 +21,10 @@ def delete_instance(request):
         result = compute.instances().list(project=project, zone=zone).execute()
         return result['items'] if 'items' in result else None
         # [END list_instances]
-
+    sql = "insert into newtable (instanceName,operation,zone) values (%s,%s,%s)"
+    val = (name,"DELETE",zone)
+    mycursor.execute(sql, val)
+    mydb.commit()
     x = list_instances(compute,'basic-tube-373302' ,zone)
     for i in range(len(x)):
 	        if (x[i]["name"]) == name:
